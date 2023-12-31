73d33aced183dc3793cfb10f8af5fd031fc907c5
Seb <seb-smith@live.com>
2023-12-31 16:51:55 +0000

    Add copilot keybinds

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -4,4 +4,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Copilot map
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
