711a45e9018090cc834a3ae976dfc261ce865a29
Seb <seb-smith@live.com>
2024-01-04 12:32:17 +0000

    Fix prettier not saving

--- init.lua
+++ init.lua
@@ -469,7 +469,7 @@ cmp.setup {
 }
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte" },
+  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.css", "*.scss", "*.json", "*.md", "*.html", "*.svelte" },
   callback = function()
     vim.cmd("Neoformat prettier")
   end,
