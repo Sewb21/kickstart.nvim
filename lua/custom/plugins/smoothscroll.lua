f5c4b90f700ad1ea19290611877944981c507871
Seb <seb-smith@live.com>
2024-01-20 11:02:17 +0000

    Add smooth scroll

---
+++ lua/custom/plugins/smoothscroll.lua
@@ -0,0 +1,12 @@
+return {
+  'karb94/neoscroll.nvim',
+  config = function()
+    require('neoscroll').setup {
+      -- All these keys will be mapped to their corresponding default scrolling animation
+      mappings = { '<C-u>', '<C-d>', 'zt', 'zz', 'zb' },
+      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+    }
+  end,
+}
