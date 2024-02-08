76bfa49d12a81e37444fceebf85a2bade58d2397
Seb <seb-smith@live.com>
2024-02-08 19:14:03 +0000

    add keymap to show fuzzy for references

--- lua/custom/plugins/goto-preview.lua
+++ lua/custom/plugins/goto-preview.lua
@@ -13,6 +13,19 @@ return {
       '<cmd>lua require("goto-preview").goto_preview_type_definition()<CR>',
       { noremap = true, silent = true, desc = 'Goto Preview Type Definition' }
     )
+    vim.keymap.set(
+      'n',
+      '<leader>gi',
+      '<cmd>lua require("goto-preview").goto_preview_implementation()<CR>',
+      { noremap = true, silent = true, desc = 'Goto Preview Implementation' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>gr',
+      '<cmd>lua require("goto-preview").goto_preview_references()<CR>',
+      { noremap = true, silent = true, desc = 'Goto Preview References' }
+    )
+
     require('goto-preview').setup {}
   end,
 }
