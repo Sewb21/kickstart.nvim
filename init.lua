a5a09209692953549bf400b7f4abe3474836980e
Seb <seb-smith@live.com>
2024-02-15 18:41:51 +0000

    Add jsx/tsx comment functionality

--- init.lua
+++ init.lua
@@ -163,7 +163,20 @@ require('lazy').setup({
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
 
   -- Fuzzy Finder (files, lsp, etc)
   {
