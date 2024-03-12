3a135146ac9b6beb826a08210edfcfb3de20fff2
Seb Smith <sebsmith@sebs-laptop.home>
2024-03-12 18:44:12 +0000

    Fix neotree not closing

--- lua/custom/plugins/image-preview.lua
+++ lua/custom/plugins/image-preview.lua
@@ -20,6 +20,14 @@ return {
           end,
         },
       },
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function()
+            require('neo-tree.command').execute { action = 'close' }
+          end,
+        },
+      },
     }
   end,
 }
--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -8,23 +8,11 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
-    '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
     require('neo-tree').setup {
       vim.keymap.set('n', '<leader>fe', ':Neotree toggle reveal<CR>', { desc = 'Toggle Explorer' }),
       vim.keymap.set('n', '<leader>fs', ':Neotree action=show reveal<CR>', { desc = 'Show file explorer' }),
-      event_handlers = {
-        {
-          event = 'file_opened',
-          handler = function(file_path)
-            require('neo-tree.command').execute { action = 'close' }
-          end,
-        },
-      },
-      window = {
-        width = 75,
-      },
     }
   end,
 }
